b450a9c9ab1888f6dc19cf2f68cff7e8e9ea5669
Sarah <callison.sarah@gmail.com>
2019-03-13 16:31:36 -0600

    added backgrounds, formatted text, added quotes

--- scripts/swShips.js
+++ scripts/swShips.js
@@ -1,7 +1,5 @@
 import { starships } from '../data/starships.js';
 
-//add in images, card from Bulma? starwars-visualguide.com
-
 let newShips = starships.filter( function(val) { 
     let speedStr = val.max_atmosphering_speed
     let speedIndex = speedStr.indexOf('km')
@@ -10,14 +8,13 @@ let newShips = starships.filter( function(val) {
     } 
     return val.max_atmosphering_speed !== "n/a" && val.max_atmosphering_speed !== "unknown"});
 
-console.log(newShips);
+// console.log(newShips);
 
 const fastest = newShips.sort((a,b) => a.max_atmosphering_speed - b.max_atmosphering_speed);
-console.log(fastest);
+// console.log(fastest);
 
 fastest.forEach(starship => {
     let shipsCont = document.getElementById('ships-container')
-
     let tile = document.createElement('div')
     tile.className = "tile"
     let titleElement = document.createElement('h4')
