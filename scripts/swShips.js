c02f98ef0e29202467df1618aa37c1f3764014c8
Sarah <callison.sarah@gmail.com>
2019-03-14 13:17:05 -0600

    ship pictures and formatting, changed title of people.html

--- scripts/swShips.js
+++ scripts/swShips.js
@@ -1,31 +1,95 @@
 import { starships } from '../data/starships.js';
 
-let newShips = starships.filter( function(val) { 
+let newShips = starships.filter(function (val) {
     let speedStr = val.max_atmosphering_speed
     let speedIndex = speedStr.indexOf('km')
-    if (speedIndex !==-1) {
+    if (speedIndex !== -1) {
         val.max_atmosphering_speed = speedStr.slice(0, 4)
-    } 
-    return val.max_atmosphering_speed !== "n/a" && val.max_atmosphering_speed !== "unknown"});
+    }
+    return val.max_atmosphering_speed !== "n/a" && val.max_atmosphering_speed !== "unknown"
+});
 
 // console.log(newShips);
 
-const fastest = newShips.sort((a,b) => a.max_atmosphering_speed - b.max_atmosphering_speed);
+const fastest = newShips.sort((a, b) => a.max_atmosphering_speed - b.max_atmosphering_speed);
 // console.log(fastest);
+
+const getLastNumber = url => {
+    let end = url.lastIndexOf('/')
+    let start = end - 2
+    if (url.charAt(start) === '/') {
+        start++
+    }
+    return url.slice(start, end)
+}
+
+fastest.forEach(starship => {
+    let imageURL = getLastNumber(starship.url)
+    if (starship.name === "Rebel transport"){
+        starship.imagePath = "2000px-Star_Wars_Yellow_Logo.png"
+    }
+    else if (starship.name === "CR90 corvette"){
+        starship.imagePath = "2000px-Star_Wars_Yellow_Logo.png"
+    }
+    else if (starship.name === "Star Destroyer"){
+        starship.imagePath = "2000px-Star_Wars_Yellow_Logo.png"
+    }
+    else if (starship.name === "Republic attack cruiser"){
+        starship.imagePath = "2000px-Star_Wars_Yellow_Logo.png"
+    }
+    else if (starship.name === "arc-170"){
+        starship.imagePath = "2000px-Star_Wars_Yellow_Logo.png"
+    }
+    else if (starship.name === "Trade Federation cruiser"){
+        starship.imagePath = "2000px-Star_Wars_Yellow_Logo.png"
+    }
+    else if (starship.name === "Naboo star skiff"){
+        starship.imagePath = "2000px-Star_Wars_Yellow_Logo.png"
+    }
+    else if (starship.name === "V-wing"){
+        starship.imagePath = "2000px-Star_Wars_Yellow_Logo.png"
+    }
+    else if (starship.name === "Belbullab-22 starfighter"){
+        starship.imagePath = "2000px-Star_Wars_Yellow_Logo.png"
+    }
+    else if (starship.name === "Jedi Interceptor"){
+        starship.imagePath = "2000px-Star_Wars_Yellow_Logo.png"
+    }
+    else if (starship.name === "Solar Sailer"){
+        starship.imagePath = "2000px-Star_Wars_Yellow_Logo.png"
+    }
+    else if (starship.name === "Theta-class T-2c shuttle"){
+        starship.imagePath = "2000px-Star_Wars_Yellow_Logo.png"
+    }else if (starship.name === "H-type Nubian yacht"){
+        starship.imagePath = "2000px-Star_Wars_Yellow_Logo.png"
+    }
+    else starship.imagePath = `https://starwars-visualguide.com/assets/img/starships/${imageURL}.jpg`
+})
+
+
 
 fastest.forEach(starship => {
     let shipsCont = document.getElementById('ships-container')
+    
     let tile = document.createElement('div')
     tile.className = "tile"
+
+    let shipPic = document.createElement('img')
+    shipPic.src = starship.imagePath
+    shipPic.className = shipPic
+
     let titleElement = document.createElement('h4')
-    tile.appendChild(titleElement)
     titleElement.textContent = starship.name
 
-let speedElement = document.createElement('p')
-tile.appendChild(speedElement)
-speedElement.textContent = starship.max_atmosphering_speed
-shipsCont.appendChild(tile)
+    let speedElement = document.createElement('p')
+    speedElement.textContent = starship.max_atmosphering_speed + " km"
+    
+    tile.appendChild(shipPic)
+    tile.appendChild(titleElement)
+    tile.appendChild(speedElement)
+    shipsCont.appendChild(tile)
 });
 
+console.log(fastest)
 
 
--- scripts/swpeople.js
+++ scripts/swpeople.js
@@ -1,56 +1,78 @@
 import { people } from '../data/people.js'
 
-    const tatooinePeeps = people.filter(person => person.homeworld === "https://swapi.co/api/planets/1/");
-//    console.log(tatooinePeeps);
 
- tatooinePeeps.forEach(person => {
+const tatooinePeeps = people.filter(person => person.homeworld === "https://swapi.co/api/planets/1/");
+
+
+const getLastNumber = url => {
+    let end = url.lastIndexOf('/')
+    let start = end - 2
+    if (url.charAt(start) === '/') {
+        start++
+    }
+    return url.slice(start, end)
+}
+
+
+tatooinePeeps.forEach(person => {
     if (person.name === "Luke Skywalker") {
-        person.quote = "There's not much to see. I used to live here you know. - The Empire Strikes Back"
-    } 
+        person.quote = "Luke Skywalker: There's not much to see."
+    }
     else if (person.name === "C-3PO") {
-        person.quote = "What a desolate place this is. - A New Hope"
+        person.quote = "C-3PO: What a desolate place this is."
     }
     else if (person.name === "Darth Vader") {
-        person.quote = "I don't like sand. It's coarse and rough and irritating and it gets everywhere. - Attack of the Clones"
+        person.quote = "Darth Vader: I don't like sand."
     }
     else if (person.name === "Owen Lars") {
-        person.quote = "It's only one more season. - A New Hope"
+        person.quote = "Owen Lars: It's only one more season."
     }
     else if (person.name === "Beru Whitesun lars") {
-        person.quote = "Owen, he can't stay here forever. - A New Hope"
+        person.quote = "Beru Lars: Owen, he can't stay here forever."
     }
     else if (person.name === "R5-D4") {
-        person.quote = "I have a bad motivator. - A New Hope"
+        person.quote = "R5-D4: I have a bad motivator."
     }
     else if (person.name === "Biggs Darklighter") {
-        person.quote = "This planet? Big hunk of nothing. - A New Hope"
+        person.quote = "Biggs Darklighter: This planet? Big hunk of nothing."
     }
     else if (person.name === "Anakin Skywalker") {
-        person.quote = "Some day I'm going to fly away from this place. - Phantom Menace"
+        person.quote = "Anakin Skywalker: Some day I'm going to fly away from this place."
     }
     else if (person.name === "Shmi Skywalker") {
-        person.quote = "The republic doesn't exist out here. We must survive on our own. - Phantom Menace"
+        person.quote = "Shmi Skywalker: The republic doesn't exist out here. We must survive on our own."
     }
+    else person.quote = "Cliegg Lars: Those Tuskens walk like men, but they're vicious, mindless monsters.";
 
-    else person.quote = "Those Tuskens walk like men, but they're vicious, mindless monsters. - Attack of the Clones";
- });
+    let imageURL = getLastNumber(person.url)
+    person.imagePath = `https://starwars-visualguide.com/assets/img/characters/${imageURL}.jpg`
+});
 
- console.log(tatooinePeeps);
 
-    // const homeLess = people.filter(person => person.homeworld === "https://swapi.co/api/planets/28/");
-    
-    // const getLastNumber = url => {
-    //     let end = url.lastIndexOf('/')
-    //     let start = end - 2
-    //     if (url.charAt(start) === '/'){
-    //         start++
-    //     }
-    //     return url.slice(start, end)
-    // }
+tatooinePeeps.forEach(person => {
+    let tatContainer = document.getElementById('tatContainer')
+    let tileElement = document.createElement('div')
+    tileElement.className = 'tile'
+    let figure = document.createElement('figure')
+    let figurePic = document.createElement('img')
+    let figureCap = document.createElement('figcaption')
+    let captionText = document.createTextNode(person.quote)
+
+    figurePic.src = person.imagePath
+
+    figureCap.appendChild(captionText)
+    figure.appendChild(figurePic)
+    figure.appendChild(figureCap)
+    tileElement.appendChild(figure)
+   
+    tatContainer.appendChild(tileElement)
+})
+
+
 
     // tatooinePeeps.forEach(person => {
     //     let tatListCont = document.getElementById('tatListCont')
-        
+
     //     let tatList = document.createElement('div')
     //     tatList.className = 'tatList'
     //     let tatNameElement = document.createElement('p')
@@ -61,52 +83,3 @@ import { people } from '../data/people.js'
     // });
 
 
-
-let tatListCont = document.querySelector('#tatListCont')
-    tatooinePeeps.forEach(person => {
-    let tatName = document.createElement('p')
-    tatName.textContent = person.name
-    let tatFig = document.createElement('figure')
-    let tatPic = document.createElement('img')
-    // person.tatPic.src = ""
-    let tatCaption = document.createElement('figcaption')
-    tatCaption.textContent = person.quote
-    
-    tatListCont.appendChild(tatName, tatFig, tatPic, tatCaption)
-
-    })
-    
-
-    //<div class = "picContainer">
-//set const = will have to look at example 
-//create element in for each function for Image, figure, figcaption, and set figcaption with the text content attribute
-//append the three elements to the container div
-// senator.Pic.src = senator.imgURL
-
-
-    // tatooinePeeps.forEach(person => {
-    //     let tatListCont = document.getElementById('tatListCont')
-        
-    //     let tatList = document.createElement('div')
-    //     tatList.className = 'tatList'
-    //     let tatNameElement = document.createElement('p')
-    //     tatList.appendChild(tatNameElement)
-    //     tatNameElement.textContent = person.name
-
-    //     tatListCont.appendChild(tatList)
-    // });
-    
-    // homeLess.forEach(person => {
-    //     let homeListCont = document.getElementById('#homeListCont')
-
-    //     let homeList = document.createElement('div')
-    //     homeList.className = 'homeList'
-    //     let homeNameElement = document.createElement('p')
-    //     homeList.appendChild(homeNameElement)
-    //     homeNameElement.textContent = person.name
-
-    //     homeListCont.appendChild(homeList)
-    // });
-    
-
-    
