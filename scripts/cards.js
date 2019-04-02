6efe8d5e7f1beb0aebc679bf36dcdf3c5fe2b660
Sarah <callison.sarah@gmail.com>
2019-04-01 22:37:25 -0600

    cleaned up forEach, set font in body

--- scripts/cards.js
+++ scripts/cards.js
@@ -4,7 +4,7 @@ const getLastNumber = url => {
   let end = url.lastIndexOf('/')
   let start = end - 2
   if (url.charAt(start) === '/') {
-      start++
+    start++
   }
   return url.slice(start, end)
 }
@@ -12,10 +12,8 @@ const getLastNumber = url => {
 people.forEach(person => {
   let personID = getLastNumber(person.url)
   person.id = personID
-  person.id = parseInt(person.id, 10)
-  let imageURL = getLastNumber(person.url)
-person.imagePath = `https://starwars-visualguide.com/assets/img/characters/${imageURL}.jpg`
-
+    // person.id = parseInt(person.id, 10)
+  person.imagePath = `https://starwars-visualguide.com/assets/img/characters/${personID}.jpg`
 })
 
 const allStars = people.filter(person => {
@@ -24,34 +22,34 @@ const allStars = people.filter(person => {
   }
 })
 
-  const mainContainer = document.querySelector('.cardContainer')
+const mainContainer = document.querySelector('.cardContainer')
 
-  function createCard(person) {
-    let card = document.createElement('div')
-    card.className = 'box'
-    let figure = document.createElement('figure')
-    let name = document.createElement('figcaption')
-    let image = document.createElement('img')
+function createCard(person) {
+  let card = document.createElement('div')
+  card.className = 'box'
+  let figure = document.createElement('figure')
+  let name = document.createElement('figcaption')
+  let image = document.createElement('img')
 
-    name.textContent = person.name
-    image.src = person.imagePath
+  name.textContent = person.name
+  image.src = person.imagePath
 
-    figure.appendChild(image)
-    figure.appendChild(name)
-    card.appendChild(figure)
-    mainContainer.appendChild(card)
-  }
+  figure.appendChild(image)
+  figure.appendChild(name)
+  card.appendChild(figure)
+  mainContainer.appendChild(card)
+}
 
-  allStars.forEach(person => {
-    createCard(person);
-  })
+allStars.forEach(person => {
+  createCard(person);
+})
 
-  class Card {
-    constructor(id) {
+class Card {
+  constructor(id) {
     // this.id = id,
     this.imageSRC = `https://starwars-visualguide.com/assets/img/characters/${id}.jpg`
-    }
   }
+}
 
 function addNewPlayer(obj) {
   let newCard = document.createElement('div')
@@ -60,18 +58,18 @@ function addNewPlayer(obj) {
   let newImage = document.createElement('img')
 
   newImage.src = obj.imageSRC
+  
   newFigure.appendChild(newImage)
   newCard.appendChild(newFigure)
   mainContainer.appendChild(newCard)
 }
-  
+
 const newPlayerButton = document.querySelector('button')
 
-newPlayerButton.addEventListener('click', function() {
+newPlayerButton.addEventListener('click', function () {
   let newID = document.getElementById('enterID').value
   addNewPlayer(new Card(newID))
 })
 
 
 // On the back: height and mass and species. 
-// Have input content be the id, set whatever to be over 25 for the input 
