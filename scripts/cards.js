accfbbcd5a5753073b3cfa714f308a93edc8f6cb
Sarah <callison.sarah@gmail.com>
2019-04-03 22:49:51 -0600

    added card back, card flip

--- scripts/cards.js
+++ scripts/cards.js
@@ -12,7 +12,7 @@ const getLastNumber = url => {
 people.forEach(person => {
   let personID = getLastNumber(person.url)
   person.id = personID
-    // person.id = parseInt(person.id, 10)
+  // person.id = parseInt(person.id, 10)
   person.imagePath = `https://starwars-visualguide.com/assets/img/characters/${personID}.jpg`
 })
 
@@ -25,8 +25,13 @@ const allStars = people.filter(person => {
 const mainContainer = document.querySelector('.cardContainer')
 
 function createCard(person) {
+  let scene = document.createElement('div')
+  scene.className = 'scene'
   let card = document.createElement('div')
-  card.className = 'box'
+  card.className = 'card'
+
+  let cardFront = document.createElement('div')
+  cardFront.className = 'box card__face'
   let figure = document.createElement('figure')
   let name = document.createElement('figcaption')
   let image = document.createElement('img')
@@ -36,40 +41,100 @@ function createCard(person) {
 
   figure.appendChild(image)
   figure.appendChild(name)
-  card.appendChild(figure)
-  mainContainer.appendChild(card)
+  cardFront.appendChild(figure)
+
+  let cardBack = document.createElement('div')
+  cardBack.className = 'box card__face card__face--back'
+  let imageBack = document.createElement('img')
+  let height = document.createElement('p')
+  let mass = document.createElement('p')
+
+  imageBack.src = '2000px-Star_Wars_Yellow_Logo.svg.png'
+  height.textContent = "Height: " + person.height
+  mass.textContent = "Mass: " + person.mass
+
+  cardBack.appendChild(imageBack)
+  cardBack.appendChild(height)
+  cardBack.appendChild(mass)
+
+  card.appendChild(cardFront)
+  card.appendChild(cardBack)
+  scene.appendChild(card)
+  mainContainer.appendChild(scene)
+
+  card.addEventListener('click', function () {
+    card.classList.toggle('is-flipped');
+  });
 }
 
 allStars.forEach(person => {
   createCard(person);
+  let i = person.id - 1
+  console.log(i)
 })
 
+
+
 class Card {
-  constructor(id) {
-    // this.id = id,
+  constructor(id) {}
     this.imageSRC = `https://starwars-visualguide.com/assets/img/characters/${id}.jpg`
   }
 }
 
+
+// function addNewPlayer(obj) {
+//   let newCard = document.createElement('div')
+//   newCard.className = 'box'
+//   let newFigure = document.createElement('figure')
+//   let newImage = document.createElement('img')
+
+//   newImage.src = obj.imageSRC
+
+//   newFigure.appendChild(newImage)
+//   newCard.appendChild(newFigure)
+//   mainContainer.appendChild(newCard)
+// }
+
 function addNewPlayer(obj) {
+  let scene = document.createElement('div')
+  scene.className = 'scene'
+  let card = document.createElement('div')
+  card.className = 'card'
+
   let newCard = document.createElement('div')
   newCard.className = 'box'
   let newFigure = document.createElement('figure')
   let newImage = document.createElement('img')
 
   newImage.src = obj.imageSRC
-  
+
   newFigure.appendChild(newImage)
   newCard.appendChild(newFigure)
-  mainContainer.appendChild(newCard)
+  card.appendChild(newCard)
+
+  let newCardBack = document.createElement('div')
+  newCardBack.className = 'box card__face card__face--back'
+  let newImageBack = document.createElement('img')
+
+  newImageBack.src = '2000px-Star_Wars_Yellow_Logo.svg.png'
+
+  newCardBack.appendChild(newImageBack)
+  card.appendChild(newCardBack)
+  scene.appendChild(card)
+  mainContainer.appendChild(scene)
+
+  card.addEventListener('click', function () {
+    card.classList.toggle('is-flipped');
+  });
 }
 
 const newPlayerButton = document.querySelector('button')
-
 newPlayerButton.addEventListener('click', function () {
   let newID = document.getElementById('enterID').value
   addNewPlayer(new Card(newID))
 })
 
+let i = allStars[24].id - 1
+console.log(allStars[i].name)
 
-// On the back: height and mass and species. 
+// i = person.id - 1, reference name property using i for index [i], how to reference specific object property
