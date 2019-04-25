061e02fecff36796d8dc07c79e162b973320747f
Sarah <callison.sarah@gmail.com>
2019-04-25 11:08:49 -0600

    commented css pages, fixed ships images

--- scripts/swShips.js
+++ scripts/swShips.js
@@ -9,10 +9,9 @@ let newShips = starships.filter(function (val) {
     return val.max_atmosphering_speed !== "n/a" && val.max_atmosphering_speed !== "unknown"
 });
 
-// console.log(newShips);
 
 const fastest = newShips.sort((a, b) => a.max_atmosphering_speed - b.max_atmosphering_speed);
-// console.log(fastest);
+
 
 const getLastNumber = url => {
     let end = url.lastIndexOf('/')
@@ -26,42 +25,42 @@ const getLastNumber = url => {
 fastest.forEach(starship => {
     let imageURL = getLastNumber(starship.url)
     if (starship.name === "Rebel transport"){
-        starship.imagePath = "2000px-Star_Wars_Yellow_Logo.png"
+        starship.imagePath = "images/2000px-Star_Wars_Yellow_Logo.svg.png"
     }
     else if (starship.name === "CR90 corvette"){
-        starship.imagePath = "2000px-Star_Wars_Yellow_Logo.png"
+        starship.imagePath = "images/2000px-Star_Wars_Yellow_Logo.svg.png"
     }
     else if (starship.name === "Star Destroyer"){
-        starship.imagePath = "2000px-Star_Wars_Yellow_Logo.png"
+        starship.imagePath = "images/2000px-Star_Wars_Yellow_Logo.svg.png"
     }
     else if (starship.name === "Republic attack cruiser"){
-        starship.imagePath = "2000px-Star_Wars_Yellow_Logo.png"
+        starship.imagePath = "images/2000px-Star_Wars_Yellow_Logo.svg.png"
     }
     else if (starship.name === "arc-170"){
-        starship.imagePath = "2000px-Star_Wars_Yellow_Logo.png"
+        starship.imagePath = "images/2000px-Star_Wars_Yellow_Logo.svg.png"
     }
     else if (starship.name === "Trade Federation cruiser"){
-        starship.imagePath = "2000px-Star_Wars_Yellow_Logo.png"
+        starship.imagePath = "images/2000px-Star_Wars_Yellow_Logo.svg.png"
     }
     else if (starship.name === "Naboo star skiff"){
-        starship.imagePath = "2000px-Star_Wars_Yellow_Logo.png"
+        starship.imagePath = "images/2000px-Star_Wars_Yellow_Logo.svg.png"
     }
     else if (starship.name === "V-wing"){
-        starship.imagePath = "2000px-Star_Wars_Yellow_Logo.png"
+        starship.imagePath = "images/2000px-Star_Wars_Yellow_Logo.svg.png"
     }
     else if (starship.name === "Belbullab-22 starfighter"){
-        starship.imagePath = "2000px-Star_Wars_Yellow_Logo.png"
+        starship.imagePath = "images/2000px-Star_Wars_Yellow_Logo.svg.png"
     }
     else if (starship.name === "Jedi Interceptor"){
-        starship.imagePath = "2000px-Star_Wars_Yellow_Logo.png"
+        starship.imagePath = "images/2000px-Star_Wars_Yellow_Logo.svg.png"
     }
     else if (starship.name === "Solar Sailer"){
-        starship.imagePath = "2000px-Star_Wars_Yellow_Logo.png"
+        starship.imagePath = "images/2000px-Star_Wars_Yellow_Logo.svg.png"
     }
     else if (starship.name === "Theta-class T-2c shuttle"){
-        starship.imagePath = "2000px-Star_Wars_Yellow_Logo.png"
+        starship.imagePath = "images/2000px-Star_Wars_Yellow_Logo.svg.png"
     }else if (starship.name === "H-type Nubian yacht"){
-        starship.imagePath = "2000px-Star_Wars_Yellow_Logo.png"
+        starship.imagePath = "images/2000px-Star_Wars_Yellow_Logo.svg.png"
     }
     else starship.imagePath = `https://starwars-visualguide.com/assets/img/starships/${imageURL}.jpg`
 })
